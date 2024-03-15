b2480d34967a76576e8bf47677a6dfd00736b292
Ajith-Menon <69453741+Ajith-Menon@users.noreply.github.com>
2024-03-15 02:36:03 +0200

    Update main.tf

--- staging/main.tf
+++ staging/main.tf
@@ -6,15 +6,6 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "<your-bucket-name>"
-    key            = "terraform-staging.tfstate"
-    region         = "<bucket-region>"
-    # Optional DynamoDB for state locking. See https://developer.hashicorp.com/terraform/language/settings/backends/s3 for details.
-    # dynamodb_table = "terraform-state-lock-table"
-    encrypt        = true
-    role_arn       = "arn:aws:iam::<your-aws-account-no>:role/<terraform-s3-backend-access-role>"
-  }
 }
 
 provider "snowflake" {
